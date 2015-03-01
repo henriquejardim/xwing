d712e5387039305f727c4c00597d49ea62573586
Henrique Jardim <henriquefjardim@gmail.com>
2015-03-01 00:09:52 -0300

    Add Brazilian Portuguese translation

--- javascripts/cards-fr.js
+++ javascripts/cards-fr.js
@@ -108,7 +108,8 @@
       '.randomize': 'Aléatoire',
       '.randomize-options': 'Options…',
       '.bbcode-list': 'Copiez le BBCode ci-dessous et collez-le dans votre post.<textarea></textarea>',
-      '.vertical-space-checkbox': "<input type=\"checkbox\" class=\"toggle-vertical-space\" /> Ajouter de l'espace pour les cartes d'amélioration et de dégâts lors de l'impression",
+      '.vertical-space-checkbox': "Ajouter de l'espace pour les cartes d'amélioration et de dégâts lors de l'impression <input type=\"checkbox\" class=\"toggle-vertical-space\" />",
+      '.color-print-checkbox': "Imprimer en couleur <input type=\"checkbox\" class=\"toggle-color-print\" />",
       '.print-list': '<i class="icon-print"></i>&nbsp;Imprimer',
       '.do-randomize': 'Générer',
       '#empireTab': 'Empire Galactique',
@@ -651,8 +652,8 @@
       "Intimidation": {
         text: "Tant que vous êtes au contact avec un vaisseau ennemi, réduisez de 1 la valeur d'agilité de ce vaisseau."
       },
-      "Calc???": {
-        text: "Cette carte n'a pas encore été révélée."
+      "Calculation": {
+        text: "When attacking, you may spend a focus token to change 1 of your %FOCUS% results to a %CRIT% result."
       },
       "Bodyguard": {
         text: "%FR_SCUMONLY%%LINEBREAK%Au début de la phase de combat, vous pouvez dépenser un marqueur de concentration pour choisir un vaisseau allié à portée 1 avec une valeur de pilotage supérieure à la votre. Augmentez sa valeur d'agilité de 1 jusqu'à la fin du tour."
